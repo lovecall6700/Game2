629bacd614b45624afac60c36d44daa8bf50efca
SHIRAISHI <SHIRAISHI@SH76K>
2020-08-23 14:37:32 +0900

    MonoGame 3.8 Android移植、コントローラーやキーボードがないので操作できない。Sony SO-04で起動だけ確認した。パッケージ名は仮のものを設定した。

---
+++ Game2/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Game2")]
+[assembly: AssemblyProduct("Game2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("謎の島の奥を目指せ！隠された扉を見つけろ！宝箱を集めて得点を稼げ！")]
+[assembly: AssemblyCompany("SHIRAISHI")]
+[assembly: AssemblyCopyright("Copyright (C) 2020 SHIRAISHI.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
